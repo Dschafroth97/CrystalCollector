611bc47a8e7c430e1b71170553381277db7e53d6
Donovan Schafroth <donovanschafroth@gmail.com>
2019-03-01 20:39:55 -0600

    game is functional, couple more things to adjust

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,24 @@ var loss = 0;
 var random_Num;
 var scoreDisplay = 0;
 
-// Generate random Num to try and match
-random_Num = [Math.floor(Math.random() * 90) + 30];
-
+//  wrapped entire number generator in function to make new randomNum and crystal values 
+//  when called in later conditionals
 function resetGame(){
+    var images = [
+        "assets/images/blueCrystal.png",
+        "assets/images/greenCrystal.jpg",
+        "assets/images/orangeCrystal.jpg",
+        "assets/images/redCrystal.jpg",
+    
+    ];
+    
+    scoreDisplay = 0;
+
+    // Generate random Num to try and match
+    random_Num = [Math.floor(Math.random() * 90) + 30];
+    
+    $("#crystalsEmpty").empty();
+
 
     $("#randomNum").html(random_Num);
 
@@ -21,36 +35,47 @@ function resetGame(){
                 "class": "crystals",
                 "data-random": random,
             });
-            crystals.html(random);
+            crystals.css({
+                "background-image":"url('" + images[i] + "')",
+                "background-size":"cover",
+                "max-width":"100%",
+            })
         $("#crystalsEmpty").append(crystals);
     }
 }
 
 resetGame();
 
-$(".crystals").on("click", function(){
-
+$(document).on("click", ".crystals", function() {
 
     var num = parseInt($(this).attr("data-random"));
 
     scoreDisplay += num;
+    
+    // when clicking crystal, add to scoreDisplay ..
+    $("#scoreDisplayBox").html(scoreDisplay);
 
-    if(scoreDisplay > random_Num){
+    // when scoreDisplay === randomNum increment wins, otherwise increment losses
+    if(scoreDisplay == random_Num){
         
-        loss++
-        $("#losses").html("Losses: " + loss);
-    }
-    else if(scoreDisplay === random_Num){
-        
-        win++
+        wins++;
 
         $("#wins").html("Wins: " + wins);
-    }
 
+        resetGame();
+    }
+    else if(scoreDisplay > random_Num){
+        
+        loss++;
+
+        $("#losses").html("Losses: " + loss);
+
+        resetGame();
+    }
 });
 
-// when clicking crystal, add to scoreDisplay
 
-// when scoreDisplay === randomNum increment wins, otherwise increment losses
 
-// generate new randomNum and crystal values
+
+
+
